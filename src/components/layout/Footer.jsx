29b12d60794256423b24b6ed07707cab1d5ff3db
Rishav Saigal <rishavsaigal@gmail.com>
2026-06-29 00:27:03 +0530

    feat: added external profile URLs to footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
       <div className="footer-container">
         <p>&copy; {new Date().getFullYear()} Rishav Saigal. All rights reserved.</p>
         <div className="footer-links">
-          <a href="#" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/rishav-saigal-4bb50311a/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           <a href="https://github.com/Rishav1996" target="_blank" rel="noopener noreferrer">GitHub</a>
-          <a href="#" target="_blank" rel="noopener noreferrer">Medium</a>
-          <a href="#" target="_blank" rel="noopener noreferrer">ORCID</a>
+          <a href="https://medium.com/@rishavsaigal" target="_blank" rel="noopener noreferrer">Medium</a>
+          <a href="https://orcid.org/0009-0005-5548-3016" target="_blank" rel="noopener noreferrer">ORCID</a>
         </div>
       </div>
     </footer>
